fe23c97c1fd7b9b9421c0b4ca45a5ee5cec0ff5f
Maxidum Yang <38829528+WorkHardForMoney@users.noreply.github.com>
2020-10-20 20:59:23 +0800

    Add files via upload

---
+++ 前端作品汇总/js/app.js
@@ -0,0 +1,17 @@
+
+		//显示广告
+		function showAd(){
+			$("#img1").slideDown(2000);
+			setTimeout("hideAd()",3000);
+		}
+		
+		//隐藏广告
+		function hideAd(){
+			$("#img1").slideUp(2000);
+		}
+		window.onload=fun();
+		function fun(){
+			$("#p1").html("dsad21321sa");
+		
+		}
+	
